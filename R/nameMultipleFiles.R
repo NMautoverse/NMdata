d09c9219f70852c303dbd1001e20b5b272a7ed38
Philip Delff <philip@delff.dk>
2025-06-03 08:46:49 -0400

    writeTextFile can write list of files

---
+++ R/nameMultipleFiles.R
@@ -0,0 +1,14 @@
+##' @param fn
+##' @param list.obj
+
+nameMultipleFiles <- function(fn,list.obj){
+    length.num.char <- length(list.obj)
+    submodels <- sprintf(fmt=paste0("%0",length.num.char,"d"),1:length.num.char)
+    ## pars[,submodel:=sprintf(fmt=paste0("%0",length.num.char,"d"),.GRP),by=bycols]
+    path.sim <- fnAppend(fn,submodel)
+    ## pars[,path.sim:=fnAppend(path.sim.0,submodel),by=.(ROW)]
+    ##pars[,fn.sim:=basename(path.sim)]
+    ##pars[,run.sim:=fnExtension(fn.sim,"")]
+
+    path.sim
+}
