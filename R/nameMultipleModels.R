13c0f607ed74f9ff9d02f4940c7b0218aed1f474
Philip Delff <philip@delff.dk>
2025-06-13 08:33:58 -0400

    nameMultipleModels added

--- R/nameMultipleModels.R
+++ R/nameMultipleModels.R
@@ -1,15 +1,18 @@
 ##' Create file names for multiple objects
 ##'
-##' Can currently only append a counter to file name stem.
+##' Can currently only append a counter.
 ##' 
 ##' @param fn A file name to use as the stem for new file names
 ##' @param list.obj List of objects to create file names for
 ##' @keywords internal
 
 nameMultipleFiles <- function(fn,list.obj){
+    
     length.num.char <- length(list.obj)
     submodels <- sprintf(fmt=paste0("%0",length.num.char,"d"),1:length.num.char)
-    path.sim <- fnAppend(fn,submodel)
+    paths.sim <- sapply(submodels,function(x){
+        fnAppend(fn,x)
+    })
 
-    path.sim
+    paths.sim
 }
