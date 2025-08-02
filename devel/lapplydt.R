3e72e916d8646afba08777340fc1cc2950942143
Philip Delff <philip@delff.dk>
2025-08-01 22:15:49 -0400

    support for nm variable in fun

--- devel/lapplydt.R
+++ devel/lapplydt.R
@@ -1,9 +1,12 @@
 ##' apply function of subsets of dt, return named list
 ##'
+##' 
 ##' @param data
 ##' @param by
 ##' @param fun
-##'
+##' @details the name of the current dataset can be reached with the
+##'     .nm variable.
+##' @import data.table
 ##' @keywords internal
 
 
@@ -11,14 +14,20 @@ lapplydt <- function(data,by,fun){
 
     dt.split <- split(data,by=by,drop=TRUE)
     nms.by <- names(dt.split)
-
     
-    tabs.sub.w <- lapply(nms.by,function(nm){
-        
+    res.l <- lapply(nms.by,function(nm){
         dt.m <- dt.split[[nm]]
-        with(list(nm=nm),fun(dt.m))
+        
+        ## Create a new function environment that contains `nm`
+        fun2 <- fun
+        env <- new.env(parent = environment(fun2))
+        env$nm <- nm
+        environment(fun2) <- env
+
+        fun2(dt.m)
+
     })
 
-    names(tabs.sub.w) <- nms.by
-    tabs.sub.w
+    names(res.l) <- nms.by
+    res.l
 }
