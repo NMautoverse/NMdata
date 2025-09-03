b78ca0fb15f9d985375eebe44fa5536d542bb904
Philip Delff <philip@delff.dk>
2025-09-03 08:26:51 -0400

    nicer colLabels() formatting

--- R/colLabels.R
+++ R/colLabels.R
@@ -1,9 +1,18 @@
 ##' Extract column labels as defined in SAS
 ##' @param ... See `?compareCols`
 ##' @return A data.frame with variable and their labels
-##' @seealso compareCols NMinfo 
+##' @seealso compareCols NMinfo
+##' @import data.table
 ##' @export 
 
-colLabels <- function(...){
-    compareCols(...,fun.class=function(x)attributes(x)$label)
+colLabels <- function(x,sort="alpha"){
+    ## compareCols(...,fun.class=function(x)attributes(x)$label)
+    res <- dtapply(x,function(x)attributes(x)$label)
+    
+    if(is.character(sort) && length(sort)==1 &&
+       tolower(cleanSpaces(sort))=="alpha"){
+        setorder(res ,"name")
+    }
+    setnames(res,c("name","res"),c("Column","Label"),skip_absent = T)
+    res
 }
