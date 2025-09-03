2ab38b2688fdf671a4343277792f02e8fdf4e3ed
Philip Delff <philip@delff.dk>
2025-09-03 06:57:44 -0400

    dtapply() and lapplydt() added

--- R/dtapply.R
+++ R/dtapply.R
@@ -1,10 +1,10 @@
 ##' Apply function and return a data.table
 ##'
-##' A convenience function that returns a data.table with a column
-##' representing the input values and a column with results. This is
+##' A convenience function that returns a data.table with a column##' representing the input values and a column with results. This is
 ##' still experimental and will not work for many input structures.
 ##' 
 ##' @param ... arguments passed to lapply
+##' @param value.names If supplied, setnames will be run on each element returned by lapply wit value.names as the `new` argument. 
 ##' @import data.table 
 ##' 
 ##' @details Only functions that return vectors are currently
@@ -20,28 +20,45 @@
 
 dtapply <- function(X,FUN,value.names=NULL,element.name="element",...){
     nms.x <- names(X)
+    if(is.null(nms.x)){
+        if(is.character(X)) {
+            nms.x <- X
+            } else {
+                nms.x <- as.character(1:length(X))
+            }
+    }
+    ## todo run in try
     res.list <- lapply(X,FUN,...)
+
+    ## todo make dt of elements. 
+    ## todo convert all to data.table
+    res.list <- lapply(res.list,as.data.table)
+    ## todo remove NULL and zro row elements
 
     
     ## res <- data.table(name=nms.x,res=unlist(res.list))
     
-    res.list <- lapply(res.list,as.data.table)
+
     if(!is.null(value.names)){
         res.list <- lapply(res.list,setnames,new=value.names)
     }
-    res.list <- lapply(nms.x,function(I)res.list[[I]][,name:=I])
-    res <- rbindlist(res.list,fill=TRUE)
+
+    res.list <- lapply(1:length(res.list),function(I){
+        res.list[[I]][,name:=nms.x[I]]
+        res.list[[I]]
+    })
+    
+    ## res.list <- lapply(nms.x,function(I){
+    ##     res.list[[I]][,name:=I]
+    ##     res.list[[I]]
+    ##     })
+    
+
+ res <- rbindlist(res.list,fill=TRUE)
+    
+    
+
     setnames(res,"name",element.name)
     setcolorder(res,element.name)
     res
 }
-
-###### used by collabels. But don't export. An improved version is
-###### available in NMdata/devel.
-
-## dtapply <- function(X,FUN,...){
-##     res.list <- lapply(X,FUN,...)
-##     ## dt1 <- data.table(name=names(res.list),res=unlist(res.list))
-##     dt1 <- data.table(name=names(X),res=unlist(res.list))
-##     dt1[]
-## }
